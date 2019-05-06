e9c776570904738efb82da03732bec801bbedd9e
王攀 <3052124905@qq.com>
2019-05-06 17:03:42 +0800

    first init

---
+++ src/main/java/io/cjf/lx03/Lx03Application.java
@@ -0,0 +1,13 @@
+package io.cjf.lx03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lx03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lx03Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/cjf/lx03/Lx03ApplicationTests.java
@@ -0,0 +1,16 @@
+package io.cjf.lx03;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lx03ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
